a456294027d779046aed693cf98ba3e6095143c2
James Mokua <jamieedgar02@gmail.com>
2022-04-29 22:13:37 +0300

    Random fixed

--- src/App.js
+++ src/App.js
@@ -1,34 +1,10 @@
-import React,{useState,useEffect} from 'react';
 import Quote from './components/Quote';
 
 function App() {
-  const [quote,setQuote] = useState({
-    anime: null,
-    character: null,
-    quote: null
-  });
-
-  const fetchQuote = async() =>{
-    return await fetch('https://animechan.vercel.app/api/random')
-    .then(response => response.json());
-  }
-  const generate = async() =>{
-    setQuote(await fetchQuote());
-  }
-  
-
  
-useEffect(() => {
-  async function fetchData(){
-    setQuote(await fetchQuote());
-  }
- fetchData();
-}, [])
-
   return (
     <div className="App">
-    <Quote quote={quote}/>
-     <button onClick={generate}>Generate new Quote</button>
+    <Quote/>
     </div>
   );
 }
--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react';
 
-const Quote = ({quote}) => {
+const Quote = () => {
+    const [quote,setQuote] = useState({
+        anime: null,
+        character: null,
+        quote: null
+      });
+    
+      const fetchQuote = async() =>{
+        return await fetch('https://animechan.vercel.app/api/random')
+        .then(response => response.json());
+      }
+      const generate = async() =>{
+        setQuote(await fetchQuote());
+      }
+      
+    
+     
+    useEffect(() => {
+      async function fetchData(){
+        setQuote(await fetchQuote());
+      }
+     fetchData();
+    }, [])
+    
   return (
+      <div>
     <div className='quote'>
        <div className='anime' title={quote.anime}>
             {quote.anime}
@@ -10,6 +34,9 @@ const Quote = ({quote}) => {
        {quote.quote}
         </blockquote>
     <div className='character' title={quote.character}>{quote.character}</div>
+    
+    </div>
+    <button onClick={generate}>Generate new Quote</button>
     </div>
   )
 }
